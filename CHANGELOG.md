60526fe654a89fdc55e13729df325e3ae2ffd380
ainame <s.namai.09@gmail.com>
2016-10-16 00:28:38 +0900

    Init commit

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+`WebP` adheres to [Semantic Versioning](http://semver.org/).
+
+## [Master](https://github.com/awesome_octocat/WebP)
+### Added
+
+### Changed
+
+### Removed
---
+++ Source/WebP.h
@@ -0,0 +1,19 @@
+//
+//  WebP.h
+//  WebP
+//
+//  Created by ainame on Jan 32, 2032.
+//  Copyright © 2016 satoshi.namai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebP.
+FOUNDATION_EXPORT double WebPVersionNumber;
+
+//! Project version string for WebP.
+FOUNDATION_EXPORT const unsigned char WebPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebP/PublicHeader.h>
+
+
