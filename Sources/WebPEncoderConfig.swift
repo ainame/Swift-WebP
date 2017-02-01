54e7b0c9584cefedde9c51ce823472074cd75071
ainame <s.namai.09@gmail.com>
2017-02-02 01:00:07 +0900

    Update libwebp to v0.6.0

--- Sources/WebPEncoderConfig.swift
+++ Sources/WebPEncoderConfig.swift
@@ -36,109 +36,114 @@ public struct WebPEncoderConfig: InternalRawRepresentable {
         case photo = 2
         case graph = 3
     }
-    
+
     // Lossless encoding (0=lossy(default), 1=lossless).
     public var lossless: Int = 0
-    
+
     // between 0 (smallest file) and 100 (biggest)
     public var quality: Float
-    
+
     // quality/speed trade-off (0=fast, 6=slower-better)
     public var method: Int
-    
+
     // Hint for image type (lossless only for now).
     public var imageHint: WebPImageHint = .default
-    
+
     // Parameters related to lossy compression only:
-    
+
     // if non-zero, set the desired target size in bytes.
     // Takes precedence over the 'compression' parameter.
     public var targetSize: Int = 0
-    
+
     // if non-zero, specifies the minimal distortion to
     // try to achieve. Takes precedence over target_size.
     public var targetPSNR: Float = 0
-    
+
     // maximum number of segments to use, in [1..4]
     public var segments: Int
-    
+
     // Spatial Noise Shaping. 0=off, 100=maximum.
     public var snsStrength: Int
-    
+
     // range: [0 = off .. 100 = strongest]
     public var filterStrength: Int
-    
+
     // range: [0 = off .. 7 = least sharp]
     public var filterSharpness: Int
-    
+
     // filtering type: 0 = simple, 1 = strong (only used
     // if filter_strength > 0 or autofilter > 0)
     public var filterType: Int
-    
+
     // Auto adjust filter's strength [0 = off, 1 = on]
     public var autofilter: Int
-    
+
     // Algorithm for encoding the alpha plane (0 = none,
     // 1 = compressed with WebP lossless). Default is 1.
     public var alphaCompression: Int = 1
-    
+
     // Predictive filtering method for alpha plane.
     // 0: none, 1: fast, 2: best. Default if 1.
     public var alphaFiltering: Int
-    
+
     // Between 0 (smallest size) and 100 (lossless).
     // Default is 100.
     public var alphaQuality: Int = 100
-    
+
     // number of entropy-analysis passes (in [1..10]).
     public var pass: Int
-    
+
     // if true, export the compressed picture back.
     // In-loop filtering is not applied.
     public var showCompressed: Bool
-    
+
     // preprocessing filter:
     // 0=none, 1=segment-smooth, 2=pseudo-random dithering
     public var preprocessing: Int
-    
+
     // log2(number of token partitions) in [0..3]. Default
     // is set to 0 for easier progressive decoding.
     public var partitions: Int = 0
-    
+
     // quality degradation allowed to fit the 512k limit
     // on prediction modes coding (0: no degradation,
     // 100: maximum possible degradation).
     public var partitionLimit: Int
-    
+
     // If true, compression parameters will be remapped
     // to better match the expected output size from
     // JPEG compression. Generally, the output size will
     // be similar but the degradation will be lower.
     public var emulateJpegSize: Bool
-    
+
     // If non-zero, try and use multi-threaded encoding.
     public var threadLevel: Int
-    
+
     // If set, reduce memory usage (but increase CPU use).
     public var lowMemory: Bool
-    
+
     // Near lossless encoding [0 = max loss .. 100 = off
     // Int(default)].
     public var nearLossless: Int = 100
-    
+
     // if non-zero, preserve the exact RGB values under
     // transparent area. Otherwise, discard this invisible
     // RGB information for better compression. The default
     // value is 0.
     public var exact: Int
-    
-    public var pad: (Int, Int, Int)
-    
+
+    // reserved for future lossless feature
+    var useDeltaPalette: Bool
+    // if needed, use sharp (and slow) RGB->YUV conversion
+    var useSharpYUV: Bool
+
+    public var pad: (Int, Int)
+
     static public func preset(_ preset: Preset, quality: Float) -> WebPEncoderConfig {
         let webPConfig = preset.webPConfig(quality: quality)
         return WebPEncoderConfig(rawValue: webPConfig)!
     }
-    
+
     internal init?(rawValue: CWebP.WebPConfig) {
         lossless = Int(rawValue.lossless)
         quality = rawValue.quality
@@ -165,13 +170,18 @@ public struct WebPEncoderConfig: InternalRawRepresentable {
         lowMemory = rawValue.low_memory != 0 ? true : false
         nearLossless = Int(rawValue.near_lossless)
         exact = Int(rawValue.exact)
-        pad = (Int(rawValue.pad.0), Int(rawValue.pad.1), Int(rawValue.pad.2))
+        useDeltaPalette = rawValue.use_delta_palette != 0 ? true : false
+        useSharpYUV = rawValue.use_sharp_yuv != 0 ? true : false
+        pad = (Int(rawValue.pad.0), Int(rawValue.pad.1))
     }
-    
+
     internal var rawValue: CWebP.WebPConfig {
         let show_compressed = showCompressed ? Int32(1) : Int32(0)
         let emulate_jpeg_size = emulateJpegSize ? Int32(1) : Int32(0)
         let low_memory = lowMemory ? Int32(1) : Int32(0)
+        let use_delta_palette = useDeltaPalette ? Int32(1) : Int32(0)
+        let use_sharp_yuv = useSharpYUV ? Int32(1) : Int32(0)
+
         return CWebP.WebPConfig(
             lossless: Int32(lossless),
             quality: Float(quality),
@@ -198,16 +208,18 @@ public struct WebPEncoderConfig: InternalRawRepresentable {
             low_memory: low_memory,
             near_lossless: Int32(nearLossless),
             exact: Int32(exact),
-            pad: (UInt32(pad.0), UInt32(pad.1), UInt32(pad.2))
+            use_delta_palette: Int32(use_delta_palette),
+            use_sharp_yuv: Int32(use_sharp_yuv),
+            pad: (UInt32(pad.0), UInt32(pad.1))
         )
     }
-    
+
     public enum Preset {
         case `default`, picture, photo, drawing, icon, text
-        
+
         func webPConfig(quality: Float) -> CWebP.WebPConfig {
             var config = CWebP.WebPConfig()
-            
+
             switch self {
             case .default:
                 WebPConfigPreset(&config, WEBP_PRESET_DEFAULT, quality)
@@ -222,9 +234,9 @@ public struct WebPEncoderConfig: InternalRawRepresentable {
             case .text:
                 WebPConfigPreset(&config, WEBP_PRESET_TEXT, quality)
             }
-            
+
             return config
         }
     }
-    
+
 }
