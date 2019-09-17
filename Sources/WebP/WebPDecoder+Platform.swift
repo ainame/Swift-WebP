0316dcab835ab80cda6ce9c15652edd03aa6e149
ainame <s.namai.09@gmail.com>
2019-09-18 00:08:31 +0100

    Use original given size when usingScaling is false

--- Sources/WebP/WebPDecoder+Platform.swift
+++ Sources/WebP/WebPDecoder+Platform.swift
@@ -9,9 +9,23 @@ extension WebPDecoder {
     public func decode(_ webPData: Data, options: WebPDecoderOptions) throws -> CGImage {
 
         let decodedData: Data = try decode(byrgbA: webPData, options: options)
-        
-        return try decodedData.withUnsafeBytes { rawPtr in
 
+        var cFeature = UnsafeMutablePointer<CWebP.WebPBitstreamFeatures>.allocate(capacity: 1)
+        defer { cFeature.deallocate() }
+
+        try webPData.withUnsafeBytes { rawPtr in
+            guard let bindedBasePtr = rawPtr.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
+                throw WebPDecodingError.unknownError
+            }
+
+            WebPGetFeatures(bindedBasePtr, webPData.count, cFeature)
+        }
+
+        let feature = WebPBitstreamFeatures(rawValue: cFeature.pointee)
+        let height: Int = options.useScaling ? options.scaledHeight : (feature?.height ?? 0)
+        let width: Int = options.useScaling ? options.scaledWidth : (feature?.width ?? 0)
+
+        return try decodedData.withUnsafeBytes { rawPtr in
             guard let bindedBasePtr = rawPtr.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
                 throw WebPDecodingError.unknownError
             }
@@ -27,11 +41,11 @@ extension WebPDecoder {
             let renderingIntent = CGColorRenderingIntent.defaultIntent
             let bytesPerPixel = 4
             
-            return CGImage(width: options.scaledWidth,
-                           height: options.scaledHeight,
+            return CGImage(width: width,
+                           height: height,
                            bitsPerComponent: 8,
                            bitsPerPixel: 8 * bytesPerPixel,
-                           bytesPerRow: bytesPerPixel * options.scaledWidth,
+                           bytesPerRow: bytesPerPixel * width,
                            space: colorSpace,
                            bitmapInfo: bitmapInfo,
                            provider: provider,
