c7c0adebb374493a665136d98198b11437045fb4
ainame <s.namai.09@gmail.com>
2024-07-14 20:27:40 +0100

    Removed outdated workflows

--- .github/bin/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh -ex
-
-set -o pipefail
-git submodule update -i
-xcodebuild -workspace "$WORKSPACE" -scheme "$SCHEME" -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO clean test | xcpretty
