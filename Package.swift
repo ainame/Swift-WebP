19d2332a3a15761dca5c1a81aad6350bed7732ec
ainame <s.namai.09@gmail.com>
2024-07-14 20:06:21 +0100

    Update swift-tools-version & supported platforms

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "WebP",
     platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v8),
-        .macOS(SupportedPlatform.MacOSVersion.v10_13)
+        .iOS(.v13),
+        .macOS(.v11)
     ],
     products: [
         .library(name: "WebP", targets: ["WebP"]),
